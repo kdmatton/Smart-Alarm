0d1ced0ba4e540be652f10202e9b71080a52182d
kdmatton <kdmatton42@tntech.edu>
2026-08-26 10:37:46 -0500

    staging files

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+// we initilize an instance of express server as app 
+const app = express()
+const port = 8000
+
+// route mount, this essentially acts as the top level of routes, and the routes directory contains 
+// route modules which we just mount on top of the top level routes which are below 
+app.use('/', require('./routes/user'))
+app.use('/auth', require('./routes/auth'))
+
+
+
+// starts the server
+app.listen(port, () => {
+    console.log(`running on port http://localhost:${port}/`)
+})
---
+++ src/routes/auth.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+// router modules, these will be mounted in app.js
+app.post('/login')
+app.post('/register')
